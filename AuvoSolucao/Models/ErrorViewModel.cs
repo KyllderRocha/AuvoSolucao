dc2b304a1b5d02df811fe3ff7ad940154aa4d71e
Kyllder Rocha <krocha@ness.com.br>
2023-02-17 08:44:03 -0300

    Adicionar arquivos de projeto.

---
+++ AuvoSolucao/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AuvoSolucao.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
